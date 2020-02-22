1e7e0f5d2754a6a7d454131f463472c2b7485c5b
Tom Wright <tomnwright@outlook.com>
2020-02-22 20:09:45 +0000

    Working Matrix Multiplication
    Finished matrix multiplication, subtraction, and addition

--- mathematics.py
+++ mathematics.py
@@ -1,10 +1,9 @@
 from math import *
+from copy import deepcopy as copy
 
-class MatrixData(list):
+class NumberArray(list):
     '''
-    Storing matrix using MatrixData
-    allows validation of individualy set matrix values.
-    eg. matrix[0][0] = 5.
+    
     '''
     def __setitem__(self, key, value):
         
@@ -15,86 +14,234 @@ class MatrixData(list):
                 'MatrixData modification: MatrixData can only contain int and float; not {}.'.format(type(value))
             )
         return
+    def dot(self, a):
+        return NumberArray.Dot(self, a)
 
+    @staticmethod
+    def Dot(a, b):
+        '''
+        Caluculates the dot product of two number arrays, a and b.
+        '''
+        bA, bB = isinstance(a, NumberArray), isinstance(b, NumberArray)
+        if not (bA and bB):
+            raise TypeError("Cannot calculate dot product of {} and {}".format(type(a), type(b)))
+        
+        L = len(a)
+        if L != len(b):
+            raise ValueError("Cannot multiply NumberArrays of different lengths.")
+
+        return sum(
+            [ ( a[i]* b[i] ) for i in range(L) ]
+        )
 class Matrix:
     '''
     2D number matrix,
-    stored as lists of MatrixData lists,
+    stored as lists of NumberArray lists,
     in column form [[column1], [column2], ...]
+    eg. 3x1 : [[1, 2, 3]]
+
+    Using NumberArray allows validation of individualy set matrix values.
+    eg. matrix[0][0] = 5.
     '''
-    def __init__(self, data):
-        self.data = data
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    def __str__(self):
-        return str(self.data)
-
     @property
     def data(self):
         return self._data
-
     @data.setter
     def data(self, value):
-        '''Set self.data value'''
-        #validate structure
-        validate = Matrix.check_data(value)
+        #CheckMatrixData will run once for every new instance. Considering removing for performance
+        validate = Matrix.Tools.CheckData(value)
 
         if validate:
             raise validate
         else:
-            self._data = [MatrixData(c) for c in value]
+            self._data = Matrix.Tools.HomoNest([NumberArray(c) for c in value])
             return
-
     @data.deleter
     def data(self):
         '''Delete self.data'''
         del self._data
 
+
+    def __init__(self, data = []):
+        self.data = data     
+    def __getitem__(self, key):
+        return self.data[key]
+    def __str__(self):
+        return str(self.data)
+
+    def __add__(self, m):
+        if not isinstance(m, Matrix):
+            raise TypeError("unsupported operand type(s) for +:  '{}' and '{}'".format(type(self).__name__, type(m).__name__))
+        
+        l = m.width == self.width
+        h = m.height == self.height
+
+        if not (l and h):
+            raise ValueError("Uncompatible matrix dimensions: {}x{} + {}x{}".format(self.height, self.width, m.height, m.width))
+        
+        
+        out = copy(self.data)
+        for i, column in enumerate(out):
+            for j, n in enumerate(column):
+
+                out[i][j] += m[i][j]
+        
+        #makes sure function works for inherited
+        outInst = type(self)()
+        outInst.data = out
+        return outInst
+    def __sub__(self, m):
+        if not isinstance(m, Matrix):
+            raise TypeError("unsupported operand type(s) for +:  '{}' and '{}'".format(type(self).__name__, type(m).__name__))
+        
+        return self + (-1 * m)
+    def __mul__(self, a):
+        if type(a) in (float, int):
+            out = copy(self.data)
+            for i, column in enumerate(out):
+                for j, n in enumerate(column):
+
+                    out[i][j] *= a
+            
+            #makes sure function works for inherited classes
+            outInst = type(self)()
+            outInst.data = out
+            return outInst
+        elif isinstance(a, Matrix):
+            #MATRIX MULTIPLICATION
+            return Matrix.Tools.Multiply(self, a)
+        else:
+            raise TypeError("unsupported operand type(s) for *: {} and {}.".format(type(self).__name__, type(a).__name__))
+    __rmul__ = __mul__
+
+
+
+    
     @property
     def width(self):
+        '''Number of columns'''
+        return len(self.data)
+    @property
+    def height(self):
+        '''Number of rows'''
         out = 0
         for c in self.data:
             if len(c) > out:
                 out = len(c)
         return out
-
-    @property
-    def height(self):
-        return len(self.data)
         
-    @classmethod
-    def check_data(cls, data):
+    @property
+    def columns(self):
         '''
-        Validates potential data set structure.
-        Returns False if data ok.
-        Returns Exception object if found.
-        ''' 
-        if not isinstance(data, list):
-            return TypeError(
-                'Matrix data must be list; not {}.'.format(type(data))
-            )
+        Returns a list of columns (class: NumberArray)
+        '''
+        return Matrix.Tools.HomoNest(self.data)
+    @property
+    def rows(self):
+        '''
+        Returns a list of rows (class: NumberArray)
+        '''
+        h = self.height
+        out = [NumberArray() for i in range(h)]
 
-        for i, column in enumerate(data):
-            
-            if not isinstance(column, list):
+        for i, col in enumerate(self.data):
+            for j, element in enumerate(col):
+                out[j].append(element)
+
+        return Matrix.Tools.HomoNest(out)
+
+    def Print(self):
+        print("\n".join([str(i) for i in self.rows]))
+
+    class Tools:
+        @staticmethod
+        def CheckData(data):
+            '''
+            Validates potential data set structure.
+            Returns False if data ok.
+            Returns Exception object if found.
+            ''' 
+            if not isinstance(data, list):
                 return TypeError(
-                    'Matrix column at position {} must be list; not {}.'.format(i, type(column))
+                    'Matrix data must be list; not {}.'.format(type(data))
                 )
 
-            for j, n in enumerate(column):
-
-                if type(n) not in (int, float,):
+            for i, column in enumerate(data):
+                
+                if not isinstance(column, list):
                     return TypeError(
-                        'Matrix data at position {},{} - Matrix can only contain int and float; not {}.'.format(i, j, type(n))
-                    )      
-        
-        return False
-    
+                        'Matrix column at position {} must be list; not {}.'.format(i, type(column))
+                    )
+
+                for j, n in enumerate(column):
+
+                    if type(n) not in (int, float,):
+                        return TypeError(
+                            'Matrix data at position {},{} - Matrix can only contain int and float; not {}.'.format(i, j, type(n))
+                        )      
+            
+            return False
+
+        @staticmethod
+        def HomoNest(nest):
+            '''
+            (Nest Homogeniser)
+            Takes list of NumberArrays (copy),
+            adds 0 to make them the same length,
+            '''
+            nList = copy(nest)
+
+            L = 0
+            homo = True
+
+            for array in nList:
+                if len(array) < L:
+                    homo = False
+                else:
+                    L = len(array)
+
+            if homo:
+                return nList
+            
+            for array in nList:
+                array += [0] * (L - len(array))
+            return nList
+
+        @staticmethod
+        def Multiply(b,a):
+
+            '''
+            Takes two Matrix instances,
+            multiplies b by a:
+            performs b(a)
+            '''
+
+            if b.width != a.height:
+                raise ValueError("Wrong Matrix dimensions for multiplication.")
+
+            nH, nW = b.height, a.width
+
+            data = []
+
+            for i in range(nW):
+                col = NumberArray()
+
+                for j in range(nH):
+
+                    val = NumberArray.Dot(
+                        b.rows[j],
+                        a.columns[i]
+                    )
+                    col.append(val)
+                
+                data.append(col)
+
+            return Matrix(data)
+
+                
 class Vector3(Matrix):
 
-    def __init__(self, x, y, z):
+    def __init__(self, x = 0, y = 0, z = 0):
         super().__init__([[x, y, z]])
     
     @property
@@ -124,7 +271,12 @@ class Vector3(Matrix):
     @property
     def magnitude(self):
         return sqrt((self.x ** 2)+(self.y ** 2)+(self.z ** 2))
-        
+    @magnitude.setter
+    def magnitude(self, value):
+        scale = value / magnitude
+        #self *= scale
+        return
+
     #standard vectors
     @property
     def unit(self):
@@ -150,8 +302,16 @@ class Vector3(Matrix):
     @property
     def down(self):
         return Vector3(0,0,-1)
+    
+    def ToMatrix(self):
+        return Matrix(self.data)
 
 if __name__ == '__main__':
     #testing
-    m = Vector3(1,2,3)
-    print(m.up)
+    m = Matrix([[1,0,0],[0,1,0],[0,0,1]])
+    n = Vector3(4,5,6)
+
+    x = (2 * m)
+    y = x * m
+    x.Print()
+    y.Print()
