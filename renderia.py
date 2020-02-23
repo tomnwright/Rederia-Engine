85501328ec4a39cf367b902e9ee404d1d26e9027
Tom Wright <tomnwright@outlook.com>
2020-02-23 15:13:50 +0000

    Settings UI Panel

--- renderia.py
+++ renderia.py
@@ -1,6 +1,16 @@
 '''
 Renderia Engine, for use within Blender.
 '''
+bl_info = {
+    "name": "Renderia",
+    "description": "Renderia Rendering Engine (Maths IA).",
+    "author": "Tom Wright",
+    "version": (1, 0),
+    "blender": (2, 81, 0),
+    "location": "Properties > Render > Renderia",
+    "category": "Render"
+}
+
 
 #Add code path to file, to allow importing modules
 import sys
@@ -16,6 +26,7 @@ import bpyconvert
 import bpy
 import bgl
 import bresenham
+
 
 class RenderiaEngine(bpy.types.RenderEngine):
     # These three members are used by blender to set up the
@@ -132,21 +143,106 @@ def get_panels():
     return panels
 
 
+
+'''
+#######################################
+##########   UI  ######################
+#######################################
+'''
+
+
+class RenderiaProperties (bpy.types.PropertyGroup):
+
+    shading: bpy.props.EnumProperty(
+        name="",
+        description="Determines the mode in which the image is rendered.",
+        items=[ ('WIREFRAME','Wireframe','Wireframe shading', 'SHADING_WIRE', 1),
+                ('SOLID','Solid','Solid shading','SHADING_SOLID', 2),
+                ('RENDERED','Rendered','Rendered (lit) shading', 'SHADING_RENDERED', 3)
+               ]
+        )
+
+class RenderiaPanelSettings:
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = "WINDOW"
+    bl_category = "Tools"
+    bl_context = "render"
+    bl_options =  {"HIDE_HEADER"}
+class RenderiaPanel(RenderiaPanelSettings, bpy.types.Panel):
+    """Creates a Panel in the scene context of the properties editor"""
+    bl_label = "Renderia"
+    bl_idname = "RENDER_PT_renderia"
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+        
+        renderiasettings = scene.renderiasettings
+
+        layout.prop(renderiasettings, "shading")
+        
+        #layout.prop_search(scene, "RenderCam", scene, "objects")
+    
+        row = layout.row()
+        row.scale_y = 2.0
+        row.operator("render.render")
+    @classmethod
+    def poll(cls, context):
+        return context.scene.render.engine == "RENDERIA"
+
+
+class RenderiaSubPanel(RenderiaPanelSettings, bpy.types.Panel):
+    """Contains settings for Renderia Panel"""
+    bl_label = "Settings"
+    bl_parent_id = "RENDER_PT_renderia"
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+        
+        renderiasettings = scene.renderiasettings
+
+
+classes  = (
+    RenderiaProperties,
+    RenderiaPanel,
+    RenderiaSubPanel,
+)
+
+
 def register():
+    from bpy.utils import register_class
+
     # Register the RenderEngine
-    bpy.utils.register_class(RenderiaEngine)
+    register_class(RenderiaEngine)
 
     for panel in get_panels():
         panel.COMPAT_ENGINES.add('RENDERIA')
 
 
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.Scene.renderiasettings = bpy.props.PointerProperty(type=RenderiaProperties)
+    return
+
+
 def unregister():
-    bpy.utils.unregister_class(RenderiaEngine)
+    from bpy.utils import unregister_class
+
+    unregister_class(RenderiaEngine)
 
     for panel in get_panels():
         if 'RENDERIA' in panel.COMPAT_ENGINES:
             panel.COMPAT_ENGINES.remove('RENDERIA')
 
+    del bpy.types.Scene.renderia
+
+    for cls in reversed(classes):
+        unregister_class(cls)
+    
 
 if __name__ == "__main__":
     register()
